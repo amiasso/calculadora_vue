132514ce4bd32ceb1f64e836c93fdf5b79249fb2
Antonio miasso <amiasso@msn.com>
2023-09-22 09:21:09 -0300

    add calculadora

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,22 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+    <div id="app">
+      <calculator></calculator>
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
-</template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+  </template>
+  
+  <script>
+  import Calculator from './components/Calculator.vue'
+  
+  export default {
+    name: 'App',
+    components: {
+      Calculator
+    }
   }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+  </script>
+  
+  <style>
+    body {
+      background: #31475e !important;
+    }
+  </style>
